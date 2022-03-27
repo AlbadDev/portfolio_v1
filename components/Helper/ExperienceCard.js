9bd20c05e4d060a2b558467d57b46ff1c3fb9cfa
Albaddev <alioubkeita95@gmail.com>
2022-03-27 16:38:32 +0000

    New: style expeerience card component

--- components/Helper/ExperienceCard.js
+++ components/Helper/ExperienceCard.js
@@ -1,14 +1,25 @@
+import style from '../../styles/ExperienceCard.module.scss'
 
-
-
-const ExperienceCard = ({work}) => {
+const ExperienceCard = ({ work }) => {
 
     return (
-        <ul>
-            <li>{work.title}</li>
-            <li>{work.description}</li>
-        </ul>
-        
+        <section className={style.sectionContainer}>
+            <div className={style.titleContainer}>
+                <h1 className={style.cardTitle}>{work.title}</h1>
+            </div>
+            <div className={style.cardParagraph}>
+                <p className={style.paragraph}>{work.description}</p>
+            </div>
+            <div className={style.technoContainer}>
+                <ul>
+                    {
+                        work.material.map( list => (
+                            <li className={style.listTechno}>{list}</li>
+                        ))
+                    }
+                </ul>
+            </div>
+        </section> 
     )
 }
 
