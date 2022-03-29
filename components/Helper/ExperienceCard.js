36d688273527d75deeb2131d4e6d1923e6de1cab
Albaddev <alioubkeita95@gmail.com>
2022-03-29 20:19:02 +0000

    New: display fetched data to component

--- components/Helper/ExperienceCard.js
+++ components/Helper/ExperienceCard.js
@@ -1,5 +1,9 @@
-import { MuiThemeProvider } from '@material-ui/core';
+import Grid  from '@material-ui/core/Grid'
 import style from '../../styles/ExperienceCard.module.scss'
+import {BsCheckCircle} from 'react-icons/bs'
+
+
+
 
 const ExperienceCard = ({ work }) => {
 
@@ -9,11 +13,9 @@ const ExperienceCard = ({ work }) => {
                 <h1 className={style.cardTitle}>{work.title}</h1>
             </div>
             <div className={style.cardParagraph}>
-                {
-                    work.mvp.map(mvp => (
-                        <p className={style.paragraph}>{mvp}</p>
-                    ))
-                }
+                <ul>
+                    { work.mvp.map(mvp =><p key={work.id} className={style.listTechno}><span><BsCheckCircle style={{color: 'cyan'}}/></span> {mvp}</p> )}
+                </ul>
             </div>
             <div className={style.technoContainer}>
                 <ul>
