2060f55c2b170cd9e1961991f85f8a87ce77560a
Albaddev <alioubkeita95@gmail.com>
2022-03-30 18:49:34 +0000

    Update: new styled component for persoWork

--- components/Helper/ExperienceCard.js
+++ components/Helper/ExperienceCard.js
@@ -8,24 +8,25 @@ import {BsCheckCircle} from 'react-icons/bs'
 const ExperienceCard = ({ work }) => {
 
     return (
-        <section className={style.sectionContainer}>
-            <div className={style.titleContainer}>
-                <h1 className={style.cardTitle}>{work.title}</h1>
+        <section className={style.experienceCard}>
+            <div className={style.workHeader}>
+                <h2 className={style.workTitle}>{work.title}</h2>
+                <a href='' className={style.workView}>view</a>
             </div>
-            <div className={style.cardParagraph}>
-                <ul>
-                    { work.mvp.map(mvp =><p key={work.id} className={style.listTechno}><span><BsCheckCircle style={{color: 'cyan'}}/></span> {mvp}</p> )}
-                </ul>
+            <div className={style.workDescription}>
+                {work.mvp.map( mvp => (
+                    <ul className={style.listContainer}>
+                        <li className={style.listOfMvp}><BsCheckCircle className={style.listIcon}/> {mvp}</li>
+                    </ul>
+                ))}
             </div>
             <div className={style.technoContainer}>
-                <ul>
-                    {
-                        work.material.map( list => (
-                            <li className={style.listTechno}>{list}</li>
-                        ))
-                    }
-                </ul>
-            </div>
+                {work.material.map( material => (
+                    <ul className={style.listContainer}>
+                        <li className={style.listOfTechno}>{material}</li>
+                    </ul>
+                ))}
+            </div>         
         </section> 
     )
 }
